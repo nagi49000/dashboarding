ab60973dcd8d8f8c87e0f76956cb641d6ef953a4
nagi <nagi>
2023-12-24 22:06:36 +0000

    flake8 fixes

--- services/streamlit/test/test_data.py
+++ services/streamlit/test/test_data.py
@@ -2,6 +2,12 @@ from app.data import get_data
 
 
 def test_run_import():
-    df = get_data(n_results=3)
-    assert len(df) == 3
+    n_row = 30
+    df = get_data(n_results=n_row)
+    assert len(df) == n_row
     assert list(df.columns) == ["lat", "lon"]
+    for row in df.iterrows():
+        # latitude should be in [-90, 90] degrees
+        assert -90.0 <= row[0] <= 90.0
+        # longitude should be in [-180, 180] degrees
+        assert -180.0 <= row[0] <= 180.0
