c40df951c84fad14695d53fbb524087c5011f00d
nagi <nagi>
2023-12-25 17:00:40 +0000

    added tests for update of records

--- services/streamlit/app/app.py
+++ services/streamlit/app/app.py
@@ -3,16 +3,20 @@ from data import get_data
 
 
 def run_app():
-    n_new_coords = "10"
+    st.session_state.n_new_coords = "10"
 
     def click_button():
-        st.session_state.coords = get_data(int(n_new_coords))
+        st.session_state.coords = get_data(int(st.session_state.n_new_coords))
 
     if "coords" not in st.session_state:
         click_button()
 
-    n_new_coords = st.text_input("Number of coords to fetch on update", "10")
-    st.button("Click to update coords", on_click=click_button)
+    st.session_state.n_new_coords = st.text_input(
+        "Number of coords to fetch on update", "10", key="n_new_coords_textbox"
+    )
+    st.button(
+        "Click to update coords", on_click=click_button, key="update_coords_button"
+    )
     st.map(st.session_state.coords)
 
 
--- services/streamlit/test/test_app.py
+++ services/streamlit/test/test_app.py
@@ -7,9 +7,22 @@ from pathlib import Path
 def test_app():
     at = AppTest.from_file(str(Path("..") / "app" / "app.py"))
     at.run(timeout=10)
+    assert not at.exception
     return at
 
 
 def test_app_start(test_app):
-    assert not test_app.exception
     assert len(test_app.session_state.coords) == 10
+
+
+def test_app_update_coords(test_app):
+    assert len(test_app.session_state.coords) == 10
+    # list of element classes to access in AppTest
+    # https://docs.streamlit.io/library/api-reference/app-testing
+    # enter the new value in the box
+    test_app.text_input(key="n_new_coords_textbox").input("5").run()
+    assert test_app.text_input(key="n_new_coords_textbox").value == "5"
+    assert test_app.session_state.n_new_coords == "5"
+    # click the update button to update the coords
+    test_app.button(key="update_coords_button").click().run()
+    assert len(test_app.session_state.coords) == 5
