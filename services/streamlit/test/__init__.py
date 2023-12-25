a6fd24ed3784d7604eb480f53519c34f750970ac
nagi <nagi>
2023-12-25 17:16:14 +0000

    removed redundant init file

--- services/streamlit/test/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys
-from pathlib import Path
-
-
-# have to add a linker path to the streamlit script for the app
-# since all imports in that script are based on the script location
-sys.path.append(str(Path(__file__).parents[1]))
