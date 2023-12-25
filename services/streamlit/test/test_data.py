b94cfcf114930eb8ec14237370b9a8e69e3086f6
nagi <nagi>
2023-12-25 17:45:08 +0000

    added test for failure to reach API

--- services/streamlit/test/test_data.py
+++ services/streamlit/test/test_data.py
@@ -1,3 +1,5 @@
+import pytest
+import re
 from app.data import get_data
 
 
@@ -11,3 +13,11 @@ def test_run_import():
         assert -90.0 <= row[0] <= 90.0
         # longitude should be in [-180, 180] degrees
         assert -180.0 <= row[0] <= 180.0
+
+
+def test_randomuser_api_unreachable(requests_mock):
+    n_row = 10
+    url = f"https://randomuser.me/api/?results={n_row}"
+    requests_mock.get(url, status_code=404)
+    with pytest.raises(RuntimeError, match=re.escape(f"request to {url} failed")):
+        get_data(n_results=n_row)
