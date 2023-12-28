24f6d97223eba9a2335fa54d945a0ac4f253587d
nagi <nagi>
2023-12-28 13:03:06 +0000

    created healthcheck endpoint and blacked code

--- services/dash/app/app.py
+++ services/dash/app/app.py
@@ -1,4 +1,5 @@
 import dash
+import json
 import plotly.express as px
 from app.data import get_data
 
@@ -6,15 +7,27 @@ from app.data import get_data
 def get_app(server=True):
     app = dash.Dash(__name__, server=server)
 
-    app.layout = dash.html.Div([
-        dash.html.H1(children='Title of Dash App', style={'textAlign': 'center'}),
-        dash.dcc.Input(id="input-n-coords", type="number", min=1, placeholder="number of coords to fetch"),
-        dash.dcc.Graph(id="graph-content")
-    ])
+    @app.server.route("/ping")
+    def ping():
+        return json.dumps({"status": "ok"})
+
+    app.layout = dash.html.Div(
+        [
+            dash.html.H1(
+                children="Map plotting with Dash", style={"textAlign": "center"}
+            ),
+            dash.dcc.Input(
+                id="input-n-coords",
+                type="number",
+                min=1,
+                placeholder="number of coords to fetch",
+            ),
+            dash.dcc.Graph(id="graph-content"),
+        ]
+    )
 
     @dash.callback(
-        dash.Output('graph-content', 'figure'),
-        dash.Input('input-n-coords', 'value')
+        dash.Output("graph-content", "figure"), dash.Input("input-n-coords", "value")
     )
     def update_graph(value):
         df = get_data(n_results=value)
