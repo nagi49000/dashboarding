9188a997850a83f8988173e293e045be1b254a7e
nagi <nagi>
2024-01-06 18:10:46 +0000

    started work on terraform for web app

---
+++ deployment/terraform/azure/webapp/dash/variables.tf
@@ -0,0 +1,10 @@
+## main login
+variable subscription_id {
+  type = string
+  description = "Azure Subscription Id"
+}
+
+variable tenant_id {
+  type = string
+  description = "Azure Tenant Id"
+}
